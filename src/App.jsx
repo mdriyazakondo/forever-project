73523c6ebe66ce7c7129c325096e328a744a3417
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-09-12 21:42:17 +0600

    check the code

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]  hidden md:block">
       <ToastContainer />
       <Navbar />
       <SearchBar />
